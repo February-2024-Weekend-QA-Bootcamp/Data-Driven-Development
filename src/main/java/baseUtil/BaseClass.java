52ef3fd6938d3aa90f4fb6d88e7f9751ea40157a
TofaelKabir <tofaelkabir@gmail.com>
2024-06-22 10:53:04 -0400

    TDD Master Branch

---
+++ src/main/java/baseUtil/BaseClass.java
@@ -0,0 +1,85 @@
+package baseUtil;
+
+import java.time.Duration;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import pages.ForgotUserId;
+import pages.HomePage;
+import pages.NewUserRegistration;
+import utils.Configuration;
+import static utils.IConstant.*;
+
+public class BaseClass {
+	public WebDriver driver;
+	public HomePage homePage;
+	public NewUserRegistration newUserRegistration;
+	public ForgotUserId forgotUserId;
+	public WebDriverWait wait;
+	Configuration configuration;
+
+	@BeforeMethod
+	public void setUp() {
+		configuration = new Configuration();
+		initDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.get(configuration.getProperties(URL));
+		long pageLoadWait = Long.parseLong(configuration.getProperties(PAGELOAD_WAIT)); // we converted String to long type
+		long implicitlyWait = Long.parseLong(configuration.getProperties(IMPLICITLY_WAIT));
+		long explicitlyWait = Long.parseLong(configuration.getProperties(EXPLICITLY_WAIT));
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(pageLoadWait));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(implicitlyWait));
+		// wait = new WebDriverWait(driver, Duration.ofSeconds(explicitlyWait));
+		initClass();
+	}
+
+	public void initDriver() {
+		String browserName = configuration.getProperties(BROWSER);
+
+		switch (browserName) {
+		case CHROME:
+			System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+			driver = new ChromeDriver();
+			break;
+
+		case FIREFOX:
+			System.setProperty("webdriver.gecko.driver", "./driver/geckodriver.exe");
+			driver = new FirefoxDriver();
+			break;
+
+		case EDGE:
+			System.setProperty("webdriver.edge.driver", "./driver/msedgedriver.exe");
+			driver = new EdgeDriver();
+			break;
+
+		default:
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+			break;
+		}
+	}
+
+	public void initClass() {
+		homePage = new HomePage(driver);
+		newUserRegistration = new NewUserRegistration(driver);
+		forgotUserId = new ForgotUserId(driver);
+	}
+
+	@AfterMethod
+	public void tearUp() {
+		driver.quit();
+	}
+
+	// create config.properties file in src/main/resoures
+	// create utils package
+	// Inside it, create enum Constant, Interface IConstant, Configuration class
+	// Bring changes in Base class
+	// static import necessary for ---> import static utils.IConstant.*
+
+}
---
+++ src/main/java/common/CommonActions.java
@@ -0,0 +1,206 @@
+package common;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import reports.Loggers;
+
+public class CommonActions {
+	public WebDriver driver;
+
+	// common method for click ()
+	public static void clickElement(WebElement element) {
+		try {
+			element.click();
+			Loggers.logTheTest(element + "<---------> has been clicked");
+		} catch (NoSuchElementException | NullPointerException e) {
+			// e.printStackTrace();
+			System.out.println("Exception is: " + e);
+			Loggers.logTheTest(element + "<----------> has not been found\n" + e.getMessage());
+			// getMessage() Returns the detail message string of this throwable.
+		}
+	}
+
+	// common method for sendKeys()
+	public static void inputText(WebElement element, String input) {
+		try {
+			element.sendKeys(input);
+			Loggers.logTheTest(input + " <-----> has been put into <-----> " + element);
+		} catch (NoSuchElementException | NullPointerException e) {
+			e.printStackTrace();
+			Loggers.logTheTest(element + "<----------> has not been found\n" + e.getMessage());
+		}
+	}
+
+	// common method for sleep()
+	public static void pause(long sec) {
+		try {
+			Thread.sleep(sec * 1000);
+			Loggers.logTheTest("Sleeping ... zZz " + sec);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+			Loggers.logTheTest("Sleep interrupted");
+		}
+	}
+
+	public static void elementDisplayed(WebElement element) {
+		try {
+			boolean flag = element.isDisplayed();
+			Loggers.logTheTest(element + "<---------> is Displayed, " + flag);
+		} catch (NoSuchElementException | NullPointerException e) {
+			e.printStackTrace();
+			Loggers.logTheTest(element + "<----------> is not Displayed\n" + e.getMessage());
+		}
+	}
+
+	public static void elementEnabled(WebElement element) {
+		try {
+			boolean flag = element.isEnabled();
+			Loggers.logTheTest(element + "<---------> is Enabled, " + flag);
+		} catch (NoSuchElementException | NullPointerException e) {
+			e.printStackTrace();
+			Loggers.logTheTest(element + "<----------> is not Displayed\n" + e.getMessage());
+		}
+	}
+
+	public static void elementSelected(WebElement element) {
+		try {
+			boolean flag = element.isSelected();
+			Loggers.logTheTest(element + "<---------> is Selected, " + flag);
+		} catch (NoSuchElementException | NullPointerException e) {
+			e.printStackTrace();
+			Loggers.logTheTest(element + "<----------> is not Displayed\n" + e.getMessage());
+		}
+	}
+
+	// I will think to change it
+	public static void verifyTitle(WebDriver driver, String expectedTitle) {
+		try {
+			String actualTitle = driver.getTitle();
+			Loggers.logTheTest("Actual Title is : " + actualTitle + " ---> And Expected Title is :" + expectedTitle);
+			Assert.assertEquals(actualTitle, expectedTitle);
+		} catch (NullPointerException e) {
+			e.printStackTrace();
+			Loggers.logTheTest("Driver is not initiated properly Or Home Page Title doesn't match");
+			Assert.fail();
+		}
+	}
+
+	public static void verifyTextOfTheWebElement(WebElement element, String expected) {
+		String actual = element.getText();
+		Loggers.logTheTest(element + " ---> Actual text : " + actual + ". Expected text : " + expected);
+		Assert.assertEquals(actual, expected, "Text In the WebElement doesn't match");
+	}
+
+	public static void verifyCurrentUrl(WebDriver driver, String expectedURL) {
+		String currentURL = driver.getCurrentUrl();
+		Loggers.logTheTest("Current URL : " + currentURL + ", Expected URL : " + expectedURL);
+		Assert.assertEquals(currentURL, expectedURL, "Current URL is not correct");
+	}
+
+	public static void clearTextFromTheField(WebElement element) {
+		try {
+			element.clear();
+			Loggers.logTheTest("The Text from the " + element + " ---> is cleared");
+		} catch (NoSuchElementException | NullPointerException e) {
+			e.printStackTrace();
+			Loggers.logTheTest(element + "<----------> has not been found\n" + e.getMessage());
+			Assert.fail();
+		}
+	}
+
+	public static void inputTextThenClickEnter(WebElement element, String input) {
+		try {
+			element.sendKeys(input, Keys.ENTER);
+			Loggers.logTheTest(
+					input + " <-----> has been put into <-----> " + element + " and then clicked by Enter Key");
+		} catch (NoSuchElementException | NullPointerException e) {
+			e.printStackTrace();
+			Loggers.logTheTest(element + "<----------> has not been found\n" + e.getMessage());
+			Assert.fail();
+		}
+	}
+
+	public static void inputTextThenClickReturn(WebElement element, String input) {
+		try {
+			element.sendKeys(input, Keys.RETURN);
+			Loggers.logTheTest(
+					input + " <-----> has been put into <-----> " + element + " and then clicked by Enter Key");
+		} catch (NoSuchElementException | NullPointerException e) {
+			e.printStackTrace();
+			Loggers.logTheTest(element + "<----------> has not been found\n" + e.getMessage());
+			Assert.fail();
+		}
+	}
+
+	public static void inputTextThenClickTab(WebElement element, String input) {
+		try {
+			element.sendKeys(input, Keys.TAB);
+			Loggers.logTheTest(
+					input + " <-----> has been put into <-----> " + element + " and then clicked by Enter Key");
+		} catch (NoSuchElementException | NullPointerException e) {
+			e.printStackTrace();
+			Loggers.logTheTest(element + "<----------> has not been found\n" + e.getMessage());
+			Assert.fail();
+		}
+	}
+
+	public static void validationOfHeader(WebElement element, String expectedHeader) {
+		String actualHeader = element.getText();
+		Assert.assertEquals(actualHeader, expectedHeader, "Header doesn't match");
+		Loggers.logTheTest(element + " ---> Actual Header : " + actualHeader + ". Expected Header : " + expectedHeader);
+	}
+
+	public static void validationOfSubHeader(WebElement element, String expectedSubHeader) {
+		String actualSubHeader = element.getText();
+		Assert.assertEquals(actualSubHeader, expectedSubHeader, "Sub Header doesn't match");
+		Loggers.logTheTest(element + " ---> Actual Sub Header : " + actualSubHeader + ". Expected SubHeader : "
+				+ expectedSubHeader);
+	}
+
+	public static void selectDropdown(WebElement element, String value) {
+		try {
+			Select select = new Select(element);
+			select.selectByVisibleText(value);
+			Loggers.logTheTest(value + " has been selected from the dropdown of ---> " + element);
+		} catch (NullPointerException | NoSuchElementException e) {
+			e.printStackTrace();
+			Loggers.logTheTest(element + " : This element Not Found");
+			Assert.fail();
+		}
+	}
+
+	public boolean isPresent(By locator) {
+		List<WebElement> elements = driver.findElements(locator);
+		if (elements.size() != 0) {
+			Loggers.logTheTest(elements + " --- > This element is present and has match of : " + elements.size());
+			return true;
+		} else {
+			Loggers.logTheTest(elements + " --- > This element is NOT present and no match found : " + elements.size());
+			return false;
+		}
+	}
+
+	public static void selectDropdownOnebyOne(WebElement element, List<WebElement> elements) {
+		try {
+			Select select = new Select(element);
+			for (int i = 1; i < elements.size(); i++) {
+				Loggers.logTheTest(elements.get(i).getText() + " is present in the dropdown");
+				select.selectByIndex(i);
+				pause(2);
+			}
+			Loggers.logTheTest("Total Element: " + (elements.size() - 1) + " is present in the dropdown");
+		} catch (NullPointerException | NoSuchElementException e) { // elements er exception add korte hobe
+			e.printStackTrace();
+			Loggers.logTheTest(element + " : This element Not Found");
+			Assert.fail();
+		}
+	}
+
+}
